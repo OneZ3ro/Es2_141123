abb6fedd48c47cd0d9968952b23bcab5b15f8218
Angelo Moreno <emilioangelo665@gmail.com>
2023-11-14 15:34:47 +0100

    Update UtentiController.java

--- src/main/java/angelomoreno/Es2_141123/controllers/UtentiController.java
+++ src/main/java/angelomoreno/Es2_141123/controllers/UtentiController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,12 +27,29 @@ public class UtentiController {
         return utenteService.getUtenti(page, size, id);
     }
 
+    @GetMapping("/me")
+    public UserDetails getProfiloUtente(@AuthenticationPrincipal Utente currentUtente) {
+        return currentUtente;
+    }
+
+    @PutMapping("/me")
+    public UserDetails modProfiloUtente(@AuthenticationPrincipal Utente currentUtente, @RequestBody Utente body) {
+        return utenteService.modificaUtente(currentUtente.getUtenteId(), body);
+    }
+
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delProfiloUtente(@AuthenticationPrincipal Utente currentUtente) {
+        utenteService.eliminaUtente(currentUtente.getUtenteId());
+    }
+
     @GetMapping("/{id}")
     public Utente findById(@PathVariable int id) {
         return utenteService.findById(id);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public Utente modificaUtente(@PathVariable int id, @RequestBody Utente body) {
         return utenteService.modificaUtente(id, body);
     }
